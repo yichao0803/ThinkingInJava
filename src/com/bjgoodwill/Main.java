6b1a33c0bd2456174c49ac7a178092cd379b8975
ZhangYichao <yichao0803@qq.com>
2018-08-29 16:21:01 +0800

    增加第三章内容 operators/*.java

--- src/com/bjgoodwill/Main.java
+++ src/com/bjgoodwill/Main.java
@@ -2,12 +2,33 @@ package com.bjgoodwill;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		StaticTest st1 = new StaticTest();
 		StaticTest st2 = new StaticTest();
 
 		System.out.println(st1.i);
 		System.out.println(st2.i);
-	// write your code here
-    }
+
+		int a = 47, b = 10;
+		System.out.println("-------");
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+		System.out.println("-------");
+		int x = -a;
+		System.out.println("x: " + x);
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+		System.out.println("-------");
+		int x1 = a * -b;
+		System.out.println("x1: " + x1);
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+		System.out.println("-------");
+		int x2 = a * (-b);
+		System.out.println("x2: " + x2);
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+		System.out.println("-------");
+
+	}
 }
