1cbebd6feda5a226cc38574228af342823765368
ZhangYichao <yichao0803@qq.com>
2018-09-02 15:08:03 +0800

    增加文件夹access 第六章 访问权限控制 #2

---
+++ src/com/bjgoodwill/access/SingleImport.java
@@ -0,0 +1,8 @@
+package com.bjgoodwill.access;//: access/SingleImport.java
+import java.util.ArrayList;
+
+public class SingleImport {
+  public static void main(String[] args) {
+    ArrayList list = new ArrayList();
+  }
+} ///:~
---
+++ src/com/bjgoodwill/access/mypackage/MyClass.java
@@ -0,0 +1,6 @@
+//: access/mypackage/MyClass.java
+package com.bjgoodwill.access.mypackage;
+
+public class MyClass {
+  // ...
+} ///:~
