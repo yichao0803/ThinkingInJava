e39c485f19cc0c24e16663bef0ecd2c373b51ddf
ZhangYichao <yichao0803@qq.com>
2018-09-02 14:42:39 +0800

    增加文件夹initialization 第五章 初始化与清理

---
+++ src/com/bjgoodwill/initialization/Measurement.java
@@ -0,0 +1,9 @@
+package com.bjgoodwill.initialization;
+
+//: initialization/Measurement.java
+class Depth {}
+
+public class Measurement {
+  Depth d = new Depth();
+  // ...
+} ///:~
---
+++ src/com/bjgoodwill/initialization/Spoon.java
@@ -0,0 +1,9 @@
+package com.bjgoodwill.initialization;
+
+//: initialization/Spoon.java
+public class Spoon {
+  static int i;
+  static {
+    i = 47;
+  }
+} ///:~
